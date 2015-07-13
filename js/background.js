ea602d9feb09b201c79dc301a8ebb76ef7a3b24e
zzp <zhangzpp@mail.ustc.edu.cn>
2015-07-13 16:56:24 +0800

    add next and previous detect

--- js/background.js
+++ js/background.js
@@ -1,3 +1,4 @@
+localStorage["site"] ="163";
 chrome.commands.onCommand.addListener(function(command) {
     if (command == 'play') {
         tabDo(function(tab){
@@ -12,6 +13,41 @@ chrome.commands.onCommand.addListener(function(command) {
                       break;
                 case "baidu":
                       action = '.play';
+                      break;
+            }
+            chrome.tabs.sendMessage(tab, action);
+        });
+    } else if (command == 'next') {
+        tabDo(function(tab){
+            var site = localStorage["site"];
+            var action;
+            switch (site) {
+                case "163":
+                      action= '.nxt';
+                      break;
+                case "qq":
+                      action = '.next_bt';
+                      break;
+                case "baidu":
+                      action = '.next';
+                      break;
+            }
+            chrome.tabs.sendMessage(tab, action);
+        });
+
+    } else if (command == 'prev') {
+        tabDo(function(tab){
+            var site = localStorage["site"];
+            var action;
+            switch (site) {
+                case "163":
+                      action= '.prv';
+                      break;
+                case "qq":
+                      action = '.prev_bt';
+                      break;
+                case "baidu":
+                      action = '.prev';
                       break;
             }
             chrome.tabs.sendMessage(tab, action);
